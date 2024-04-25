e6494711dbdd0322430556c289ff07cdec37de02
kiggsmk <116185751+kiggsmk@users.noreply.github.com>
2024-04-25 11:16:30 +0530

    who are we component added to home

--- src/home/home.js
+++ src/home/home.js
@@ -1,12 +1,13 @@
 import styles from './home.module.css';
-import Title from './title.js';
+import Who from './components/who';
 
 
 
 export default function Home(){
     return(
         <><div className={styles.homecont}>
-             <div className={styles.titlecont}><Title/></div>
+             <div className={styles.titlecont}></div>
+            <Who/>
         </div>
         </>
     )
