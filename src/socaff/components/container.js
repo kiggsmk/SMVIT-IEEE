30330ffecc1f71e57a1a68cd1c99825c3e4ce5a3
kiggsmk <116185751+kiggsmk@users.noreply.github.com>
2024-04-27 23:23:13 +0530

    updated societies and aff

---
+++ src/socaff/components/container.js
@@ -0,0 +1,53 @@
+import styles from './soc.module.css';
+import Card from '../../home/components/card';
+import React, { useState } from 'react';
+
+export default function Soccont(props){
+
+    const [hidden, setHidden] = useState(true);
+    
+    
+    var R=(hidden ? "more" : "less");
+        
+
+    return(
+        <div className={(hidden ? styles.soccardparent : styles.soccardparentunhidden)}>
+            <div className={styles.upcont}>
+                <div className={styles.upleftcont}>
+                    <img className={styles.upleftimg} alt={props.name} src={props.socimg}/>
+                </div>
+                <div className={styles.uprightcont}>
+                    <div className={styles.uprightheadcont}>
+                        <h1 className={styles.uprighthead}>{props.name}</h1>
+                    </div>
+                    <div className={styles.uprighttextcont}>
+                        <p className={styles.uprighttext}>{props.desc}</p>
+                        <button className={styles.morebut} onClick={() => setHidden(!hidden)}>Read {R} → </button>
+                    </div>
+                </div>
+            </div>
+
+            <hr/>
+
+            <div className={styles.hiddensoccont}>
+                <div className={styles.downcont}>
+                    <div className={styles.downleftcont}>
+                        <img className={styles.downleftimg} src={props.rep}/>
+                    </div>
+                    <div className={styles.downrightcont}>
+                        <div className={styles.downrightheadcont}>
+                            <h1 className={styles.downrighthead}>Meet the Team!</h1>
+                        </div>
+                        <div className={styles.downrighttextcont}>
+                            <p className={styles.downrighttext}>{props.teamdesc}</p>
+                        </div>
+                    </div>
+                </div>
+                {/* <div className={styles.soceventcont}>
+                    <div className={styles.cardHolder}><Card/></div>
+                    <div className={styles.cardHolder}><Card/></div>
+                </div> */}
+            </div>    
+        </div>
+    )
+}
--- src/socaff/soc.js
+++ src/socaff/soc.js
@@ -1,5 +1,39 @@
+import styles from './components/soc.module.css';
+import Soccont from './components/container';
+import rep1 from './components/demo.jpg';
+import titleimg from './components/demo2.jpg';
+
 export default function Soc(){
     return(
-        <h1>Societies and  Affiliations</h1>
+        <div className={styles.soccont}>
+            <div className={styles.soctitlecont}></div>
+            <div className={styles.socbodycont}>
+                <div className={styles.soc1}>
+                    <Soccont socimg={titleimg}
+                    name="Something society" 
+                    desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged." 
+                    rep={rep1}
+                    teamdesc="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. "/>
+                </div>
+
+                <div className={styles.soc1}>
+                    <Soccont socimg={titleimg}
+                    name="Something society" 
+                    desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged." 
+                    rep={rep1}
+                    teamdesc="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. "/>
+                </div>
+
+                <div className={styles.soc1}>
+                    <Soccont socimg={titleimg}
+                    name="Something society" 
+                    desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged." 
+                    rep={rep1}
+                    teamdesc="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. "/>
+                </div>
+
+            </div>
+        </div>
+        
     )
 }
