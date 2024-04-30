ee9bd8ce485bf7adcb2ccb4a325aaea22f613901
kiggsmk <116185751+kiggsmk@users.noreply.github.com>
2024-04-30 12:28:51 +0530

    updated membership page

--- src/membership/mem.js
+++ src/membership/mem.js
@@ -1,13 +1,21 @@
 import styles from './membership.module.css';
+import React,{useState} from 'react';
+import {NavLink} from "react-router-dom";
+import ar from './arrowright.png';
 
 export default function Membership(){
+    const [rupee, setCurrency]= useState (true);
+    function changeCurrency(){
+        setCurrency(!rupee);
+    }   
     return(
         <div className={styles.membody}>
+            <div className={styles.memtitlecont}></div>
             <div className={styles.memtopcont}>
                 <div className={styles.memtopleftcont}>
                     <div className={styles.tlc1cont}>
-                        <div className={styles.tlc1textcont}><p className={styles.tloc1text}></p></div>
-                        <div className={styles.tlc1butcont}><button type='button' className={styles.tlc1}>Reach Out!</button> </div>
+                        <div className={styles.tlc1textcont}><p className={styles.tlc1text}>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p></div>
+                        <div className={styles.tlc1butcont}><button type='button' className={styles.tlc1but}>Reach Out!</button> </div>
                     </div>
                     <div className={styles.tlc2cont}>
                         <div className={styles.tlc2headcont}><h1 className={styles.tlc2head}>Joining today will get you:</h1></div>
@@ -20,6 +28,12 @@ export default function Membership(){
                                 <li className={styles.tlc2listitem}>Something</li>
                                 <li className={styles.tlc2listitem}>Something</li>
                             </ul>
+                            <div className={styles.listimgcont}>
+                                <img src='img' alt='ieee' className={styles.listimg}/><br/>
+                                <img src='img' alt='ieee' className={styles.listimg}/><br/>
+                                <img src='img' alt='ieee' className={styles.listimg}/><br/>
+                                <img src='img' alt='ieee' className={styles.listimg}/>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -27,15 +41,81 @@ export default function Membership(){
                     <div className={styles.regheadcont}><h1 className={styles.reghead}>Register!</h1></div>
                     <div className={styles.regformcont}>
                         <form className={styles.regform} method='post' >
-                            <label className={styles.regformlabel}>Name</label>
-                            <input placeholder='Enter full name' className={styles.regforminput} name='name' type='text' required/>
-                            <label className={styles.regformlabel}>Email</label>
-                            <input placeholder='email@xyz.com' className={styles.regforminput} name='email' type='text' required/>
-                            <label className={styles.regformlabel}>Contact No.</label>
-                            <input className={styles.regforminput} name='name' type='text' />
-                            <input type="submit" value="Submit" className={styles.regformbut}/>
+                            <label className={styles.regformlabel}>Name</label><br/>
+                            <input placeholder='Enter full name' className={styles.regforminput} name='name' type='text' required/><br/>
+                            <label className={styles.regformlabel}>Email</label><br/>
+                            <input placeholder='email@xyz.com' className={styles.regforminput} name='email' type='text' required/><br/>
+                            <label className={styles.regformlabel}>Contact No.</label><br/>
+                            <input className={styles.regforminput} name='phone' type='num' /><br/>
+                            <input type="submit" value="Submit" className={styles.regformbut}/><br/>
                         </form>
                     </div>
+                </div>
+            </div>
+            <div className={styles.memfeecont}>
+                <div className={styles.memfeeheadcont}>
+                    <h1 className={styles.memfeehead}>Fee Structure</h1>
+                </div>
+                <div className={styles.feetablecont}>
+                <table className={styles.feetable}>
+                    <tr>
+                        <th>Society/Affinity</th>
+                        <th>New Member</th>
+                        <th>Renewal</th>
+                    </tr>
+                    <tr>
+                        <td>Student Membership</td>
+                        <td onClick={()=>changeCurrency()}>{rupee ? '₹':'$'}</td>
+                        <td onClick={()=>changeCurrency()}>{rupee ? '₹':'$'}</td>
+                    </tr>
+                    <tr>
+                        <td>Centro comercial Moctezuma</td>
+                        <td onClick={()=>changeCurrency()}>{rupee ? '₹':'$'}</td>
+                        <td onClick={()=>changeCurrency()}>{rupee ? '₹':'$'}</td>
+                    </tr>
+                    <tr>
+                        <td>Centro comercial Moctezuma</td>
+                        <td onClick={()=>changeCurrency()}>{rupee ? '₹':'$'}</td>
+                        <td onClick={()=>changeCurrency()}>{rupee ? '₹':'$'}</td>
+                    </tr>
+                    <tr>
+                        <td>Centro comercial Moctezuma</td>
+                        <td onClick={()=>changeCurrency()}>{rupee ? '₹':'$'}</td>
+                        <td onClick={()=>changeCurrency()}>{rupee ? '₹':'$'}</td>
+                    </tr>
+                </table>    
+                </div>
+            </div>
+            <div className={styles.memreachcont}>
+                <div className={styles.memreachheadcont}>
+                    <h1 className={styles.memreachhead}>Reach Out To Us!</h1>
+                </div>
+                <div className={styles.memreachbody}>
+                <div className={styles.butcont}>
+                    <NavLink className={styles.but} to="/socaff">
+                        <div className={styles.butsrcont}><p className={styles.butsr}>01</p></div>
+                        <div className={styles.buttextcont}><p className={styles.buttext}>Societies and  Affiliations</p></div>
+                        <div className={styles.arrowcont}><img className={styles.arrow} src={ar} alt="soc"/></div>
+                    </NavLink>
+                    </div>
+                    <hr className={styles.abbutline}/>
+                    <div className={styles.butcont}>
+                    <NavLink className={styles.but} to="/membership">
+                        <div className={styles.butsrcont}><p className={styles.butsr}>02</p></div>
+                        <div className={styles.buttextcont}><p className={styles.buttext}>Membership</p></div>
+                        <div className={styles.arrowcont}><img className={styles.arrow} src={ar} alt="soc"/></div>
+                    </NavLink>
+                    </div>
+                    <hr className={styles.abbutline}/>
+                    <div className={styles.butcont}>
+                    <NavLink className={styles.but} to="/membership">
+                        <div className={styles.butsrcont}><p className={styles.butsr}>02</p></div>
+                        <div className={styles.buttextcont}><p className={styles.buttext}>Membership</p></div>
+                        <div className={styles.arrowcont}><img className={styles.arrow} src={ar} alt="soc"/></div>
+                    </NavLink>
+                    </div>
+                    <hr className={styles.abbutline}/>
+
                 </div>
             </div>
         </div>
