9e6721afa8e32363fa8f1fd358e62514e1fb23d1
kiggsmk <116185751+kiggsmk@users.noreply.github.com>
2024-07-08 00:58:58 +0530

    gallery content upload

--- src/gallery/galpopup.js
+++ src/gallery/galpopup.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import styles from './gal.module.css';
 import SimpleImageSlider from "react-simple-image-slider";
+import close from './closeicon.png';
+import ial1 from './media/alumnitalk/i1.jpg';
+import ial2 from './media/alumnitalk/i2.jpg';
+import ial3 from './media/alumnitalk/i3.jpg';
+import ial4 from './media/alumnitalk/i4.jpg';
+import ial5 from './media/alumnitalk/i5.jpg';
+import ial6 from './media/alumnitalk/i6.jpg';
 
 
 const images = [
-  { url: "images/1.jpg" },
-  { url: "images/2.jpg" },
-  { url: "images/3.jpg" },
-  { url: "images/4.jpg" },
-  { url: "images/5.jpg" },
-  { url: "images/6.jpg" },
-  { url: "images/7.jpg" },
+  ial1, ial2, ial3, ial4, ial5, ial6
 ];
+
 
 
 
@@ -19,7 +21,7 @@ export default function GalPop(props) {
   return (
     <>
     <div className={(styles.galpopcontopen)}>
-    <button className={styles.galpopclosebut} onClick={props.handleOnClick}><img src='img'/></button>
+    <div className={styles.galpopclosebut} ><img className={styles.galpopcloseicon} onClick={props.handleOnClick} src={close}/></div>
           <SimpleImageSlider
               width={896}
               height={504}
