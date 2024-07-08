6c85050cb7b44ad92831cfa712af803ee5abf450
kiggsmk <116185751+kiggsmk@users.noreply.github.com>
2024-07-08 10:39:29 +0530

    404 page update

--- src/nopage/nopg.js
+++ src/nopage/nopg.js
@@ -1,5 +1,11 @@
+import styles from './nopage.module.css';
 export default function NoPage(){
     return(
-        <h1>Nopage</h1>
+    <div className={styles.titlecont}>
+        <div className={styles.titleheadcont}>
+            <h3 className={styles.titlehead1}>You're lost buddy</h3>
+            <h1 className={styles.titlehead2}>Page Not found</h1>
+        </div>
+    </div>
     )
 }
