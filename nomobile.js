bba46ffbd28611c30e83fd6c67c1cb628723b672
kiggsmk <116185751+kiggsmk@users.noreply.github.com>
2024-10-16 12:49:25 +0530

    Updates

---
+++ nomobile.js
@@ -0,0 +1,6 @@
+var ht= screen.availHeight;
+var wd= screen.availWidth;
+if (wd<1308){
+    alert("This Website is irresponsive to Mobile phone Devices. Please use Desktop to access the content.");
+    window.location.replace("https://www.sirmvit.edu/");
+}
